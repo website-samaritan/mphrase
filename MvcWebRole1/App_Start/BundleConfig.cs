850129e91df9dc4be70ca7095300d32f33c09ab5
mphrase <mfras3r@hotmail.com>
2014-12-17 00:56:09 -0500

    Getting rid of the assets folder for the front end. The web role is a build destination.

--- MvcWebRole1/App_Start/BundleConfig.cs
+++ MvcWebRole1/App_Start/BundleConfig.cs
@@ -8,7 +8,7 @@ namespace MvcWebRole1
 		// For more information on Bundling, visit http://go.microsoft.com/fwlink/?LinkId=254725
 		public static void RegisterBundles(BundleCollection bundles)
 		{
-			bundles.Add(new StyleBundle("~/assets/css"));
+			bundles.Add(new StyleBundle("~/css"));
 		}
 	}
 }
